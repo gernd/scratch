976976748428dd0b791e1070db8f36c2d37ca464
Gernot Pointner <gernot.pointner@jambit.com>
2019-01-05 14:32:45 +0100

    enhance fizzbuzz

--- haskell/fizzbuzz.hs
+++ haskell/fizzbuzz.hs
@@ -1,6 +1,7 @@
 fizzbuzz = mapM_ print $ map fizzbuzz_  [1..100]
     where fizzbuzz_ x 
+                | (x `mod` 3 == 0 && x `mod` 5 == 0) = "fizzbuzz"
                 | (x `mod` 3 == 0) = "fizz"
                 | (x `mod` 5 == 0) = "buzz"
-                | otherwise = x
+                | otherwise = "nope"
 
